0f84dabedc67224ba5c25818142bcb67ece539b3
jhonathanmpg <p.jhonathan@hotmail.com>
2021-06-18 19:47:24 -0300

    Mi primer pull request

--- src/main.py
+++ src/main.py
@@ -3,6 +3,16 @@ def main():
     main() -> None
     """
     return None
+def sumatoria(x):
+    resultado = (x*(x+1))/2
+    return print(resultado)
+sumatoria(3)
+
+def calVolumenParalelepipedo(x,y,z):
+    resultado = x*y*z
+    return print(resultado)
+
+calVolumenParalelepipedo(2,3,10)
 
 if __name__ == "__main__":
     main()
