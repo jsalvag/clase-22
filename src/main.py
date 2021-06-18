456f34b17d88b38491b608fa1338888b86386217
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-06-18 19:29:36 -0300

    function complex

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
+
 def main():
     """
     main() -> None
     """
+    myVariable = complex()
+    print(myVariable)
     return None
+
+# Esto sera una constante
+complex_zero = {0,0}
+
+def complex(real=0.0, imag=0.0):
+    """Form a complex number.
+
+    Keyword arguments:
+    real -- the real part (default 0.0)
+    imag -- the imaginary part (default 0.0)
+    """
+    if imag == 0.0 and real == 0.0:
+        return complex_zero
+    
 
 if __name__ == "__main__":
     main()
